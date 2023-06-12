91adbd5b935ffd7c33d245de876640f29cee97e3
Arpit Verma <arpitverma568@gmail.com>
2023-06-12 16:27:51 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
+const express = require("express")
+const cors = require("cors")
+const mongoose =  require("mongoose")
 
 const app = express()
 app.use(express.urlencoded({extended:true}))
